a4bfb707cfe463e9ee651ea39e40917e58249e04
thelastgoth128 <thindwacyrus86@gmail.com>
2026-01-27 11:10:11 +0200

    feat: generate adn set up resource organizers with nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './compoments/users/users.module';
+import { OrganizersModule } from './compoments/organizers/organizers.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, OrganizersModule],
   controllers: [AppController],
   providers: [AppService],
 })
