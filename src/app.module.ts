e94d83ae730d710776a233d0dbefd5662b5e9573
thelastgoth128 <thindwacyrus86@gmail.com>
2026-01-27 11:08:12 +0200

    feat: generate and set up resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './compoments/users/users.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
