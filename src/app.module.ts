3eeb56ee760b88a07f9cb144b2e8ea169090955a
thelastgoth128 <thindwacyrus86@gmail.com>
2026-01-27 11:11:16 +0200

    feat: setu up events resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './compoments/users/users.module';
 import { OrganizersModule } from './compoments/organizers/organizers.module';
+import { EventsModule } from './compoments/events/events.module';
 
 @Module({
-  imports: [UsersModule, OrganizersModule],
+  imports: [UsersModule, OrganizersModule, EventsModule],
   controllers: [AppController],
   providers: [AppService],
 })
