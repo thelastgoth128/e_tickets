c498cd41af98353079476a7cf157519b6bd8b9c6
thelastgoth128 <thindwacyrus86@gmail.com>
2026-01-30 01:53:52 +0200

    change port

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   app.enableCors();
-  await app.listen(process.env.PORT ?? 3004);
+  await app.listen(3004);
 }
 bootstrap();
