b9f9ac6e4912b020752ec9354996fdaffcdc6fb9
thelastgoth128 <thindwacyrus86@gmail.com>
2026-01-29 22:08:46 +0200

    feat: add TypeOrm and HttpModule imports to Refunds and Transactions modules

--- src/compoments/refunds/refunds.module.ts
+++ src/compoments/refunds/refunds.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { RefundsService } from './refunds.service';
 import { RefundsController } from './refunds.controller';
+import { Refund } from './entities/refund.entity';
+import { Transaction } from '../transactions/entities/transaction.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Refund, Transaction])],
   controllers: [RefundsController],
   providers: [RefundsService],
 })
--- src/compoments/transactions/transactions.module.ts
+++ src/compoments/transactions/transactions.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TransactionsService } from './transactions.service';
 import { TransactionsController } from './transactions.controller';
+import { HttpModule } from '@nestjs/axios';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Transaction } from './entities/transaction.entity';
 
 @Module({
+  imports: [
+    HttpModule,
+    TypeOrmModule.forFeature([Transaction]),
+  ],
   controllers: [TransactionsController],
   providers: [TransactionsService],
 })
-export class TransactionsModule {}
+export class TransactionsModule { }
