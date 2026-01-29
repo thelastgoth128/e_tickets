3706d5c15362a393f324818b109585d802d3ae5e
thelastgoth128 <thindwacyrus86@gmail.com>
2026-01-29 22:04:58 +0200

    feat: setup swagger for API doc

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('UNIMA Event Manager API')
+    .setDescription('The UNIMA Event Manager API description. \n\n**Note on Authentication:**\nThis specification does not implement actual authentication logic. Access control requirements (e.g., "Buyer only", "Organizer only") are documented in the endpoint descriptions.')
+    .setVersion('1.0')
+    .addTag('events')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
