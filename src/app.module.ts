1dcdb9b87a31a49fd217484502b96f5a2e354f8f
thelastgoth128 <thindwacyrus86@gmail.com>
2026-01-31 13:30:03 +0200

    chore: correct naming

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,17 +3,17 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './compoments/users/users.module';
-import { OrganizersModule } from './compoments/organizers/organizers.module';
-import { EventsModule } from './compoments/events/events.module';
-import { TicketTiersModule } from './compoments/ticket_tiers/ticket_tiers.module';
-import { TicketsModule } from './compoments/tickets/tickets.module';
-import { TransactionsModule } from './compoments/transactions/transactions.module';
-import { RefundsModule } from './compoments/refunds/refunds.module';
-import { WaitlistModule } from './compoments/waitlist/waitlist.module';
-import { FeedbackModule } from './compoments/feedback/feedback.module';
-import { NotificationsModule } from './compoments/notifications/notifications.module';
-import { AuditLogModule } from './compoments/audit_log/audit_log.module';
+import { UsersModule } from './components/users/users.module';
+import { OrganizersModule } from './components/organizers/organizers.module';
+import { EventsModule } from './components/events/events.module';
+import { TicketTiersModule } from './components/ticket_tiers/ticket_tiers.module';
+import { TicketsModule } from './components/tickets/tickets.module';
+import { TransactionsModule } from './components/transactions/transactions.module';
+import { RefundsModule } from './components/refunds/refunds.module';
+import { WaitlistModule } from './components/waitlist/waitlist.module';
+import { FeedbackModule } from './components/feedback/feedback.module';
+import { NotificationsModule } from './components/notifications/notifications.module';
+import { AuditLogModule } from './components/audit_log/audit_log.module';
 import { AuthModule } from './components/auth/auth.module';
 
 @Module({
