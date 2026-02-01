cf06db185e02e5f764b872b05b9b1cb795b74586
thelastgoth128 <thindwacyrus86@gmail.com>
2026-02-01 13:48:57 +0200

    feat: add authguard for verifying token

---
+++ src/components/auth/guards/authGuard.ts
@@ -0,0 +1,48 @@
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
+import { JwtService } from "@nestjs/jwt";
+import { Request } from "express";
+import { Observable } from "rxjs";
+import { IS_PUBLIC_KEY } from "./public";
+import { UsersService } from "src/components/users/users.service";
+
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+    constructor(
+        private jwtService : JwtService,
+        private reflector : Reflector,
+        private userService : UsersService
+    ){}
+
+    async canActivate(context: ExecutionContext): Promise<boolean>  {
+        const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY,[
+            context.getHandler(),
+            context.getClass()
+        ])
+
+        if (isPublic) {
+            return true;
+        }
+        const request = context.switchToHttp().getRequest()
+        const token = this.extractTokenFromHeader(request)
+
+        if (!token){
+            throw new UnauthorizedException("you have no access token");
+        }
+        try{
+            const data = await this.jwtService.verifyAsync(token,{secret:process.env.JWT_SECRET})
+
+            request['user'] = data
+
+            const userid = data
+        }catch(error){
+            throw new UnauthorizedException("access denied", error)
+        }
+        return true
+    }
+    private extractTokenFromHeader(request:Request): string | undefined{
+        const [type,token] = request.headers.authorization?.split(' ')??[]
+        return type == 'Bearer' ? token : undefined
+    }
+}
