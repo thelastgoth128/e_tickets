b3df7dc66f2a2bd5fb12e1fcdb586bcc3d80709f
thelastgoth128 <thindwacyrus86@gmail.com>
2026-02-03 19:49:48 +0200

    feat: import typeorm feature

--- src/components/organizers/organizers.module.ts
+++ src/components/organizers/organizers.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { OrganizersService } from './organizers.service';
 import { OrganizersController } from './organizers.controller';
+import { Organizer } from './entities/organizer.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [
+      TypeOrmModule.forFeature([Organizer]),
+    ],
   controllers: [OrganizersController],
   providers: [OrganizersService],
 })
