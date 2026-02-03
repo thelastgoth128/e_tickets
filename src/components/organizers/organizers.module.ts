9dbe6194a2e815cd155137c383b4d2cebcb83da0
thelastgoth128 <thindwacyrus86@gmail.com>
2026-02-03 20:56:44 +0200

    feat: import repositories

--- src/components/organizers/organizers.module.ts
+++ src/components/organizers/organizers.module.ts
@@ -3,10 +3,12 @@ import { OrganizersService } from './organizers.service';
 import { OrganizersController } from './organizers.controller';
 import { Organizer } from './entities/organizer.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from '../users/users.module';
+import { User } from '../users/entities/user.entity';
 
 @Module({
   imports: [
-      TypeOrmModule.forFeature([Organizer]),
+      TypeOrmModule.forFeature([Organizer,User]),UsersModule
     ],
   controllers: [OrganizersController],
   providers: [OrganizersService],
--- src/components/users/users.module.ts
+++ src/components/users/users.module.ts
@@ -3,10 +3,11 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { Organizer } from '../organizers/entities/organizer.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([User]),
+    TypeOrmModule.forFeature([User, Organizer]),
   ],
   controllers: [UsersController],
   providers: [UsersService],
