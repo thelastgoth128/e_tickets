b8cf752620dc41aa0380838cc42c0fd3beb09c9a
thelastgoth128 <thindwacyrus86@gmail.com>
2026-02-03 20:57:30 +0200

    chore: update dto

--- src/components/organizers/dto/create-organizer.dto.ts
+++ src/components/organizers/dto/create-organizer.dto.ts
@@ -1,15 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { User } from 'src/components/users/entities/user.entity';
 
 export class CreateOrganizerDto {
-    @ApiProperty({ example: 'Unima Events Team', description: 'Name of the organizer or organization' })
-    name: string;
-
-    @ApiProperty({ example: 'events@unima.ac.mw', description: 'Contact email' })
-    email: string;
-
-    @ApiProperty({ example: '+265 999 123 456', description: 'Contact phone number' })
-    phone: string;
-
-    @ApiProperty({ example: 'Official events organizer for the university', description: 'Description of the organizer' })
-    description: string;
+    user: User;
 }
