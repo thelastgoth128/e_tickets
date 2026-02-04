87219655c3b17a4b1903d24b9936dd6b0c54e927
thelastgoth128 <thindwacyrus86@gmail.com>
2026-02-04 22:35:30 +0200

    feat: add a new typeorm feature

--- src/components/tickets/tickets.module.ts
+++ src/components/tickets/tickets.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TicketsService } from './tickets.service';
 import { TicketsController } from './tickets.controller';
 import { Ticket } from './entities/ticket.entity';
+import { TicketTier } from '../ticket_tiers/entities/ticket_tier.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Ticket])],
+  imports: [TypeOrmModule.forFeature([Ticket, TicketTier])],
   controllers: [TicketsController],
   providers: [TicketsService],
   exports: [TicketsService],
