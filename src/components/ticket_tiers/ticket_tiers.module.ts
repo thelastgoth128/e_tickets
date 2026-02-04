b75df8a062707fcb923962796cb6a6eaea3ca16c
thelastgoth128 <thindwacyrus86@gmail.com>
2026-02-04 22:25:07 +0200

    chore: update module with tpeorm

--- src/components/ticket_tiers/ticket_tiers.module.ts
+++ src/components/ticket_tiers/ticket_tiers.module.ts
@@ -2,8 +2,13 @@ import { Module } from '@nestjs/common';
 import { TicketTiersService } from './ticket_tiers.service';
 import { TicketTiersController } from './ticket_tiers.controller';
 
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TicketTier } from './entities/ticket_tier.entity';
+import { Event } from '../events/entities/event.entity';
+
 @Module({
+  imports: [TypeOrmModule.forFeature([TicketTier, Event])],
   controllers: [TicketTiersController],
   providers: [TicketTiersService],
 })
-export class TicketTiersModule {}
+export class TicketTiersModule { }
