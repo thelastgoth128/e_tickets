bfe9d9e6515a0ec9f455db6fd9e832236780706e
thelastgoth128 <thindwacyrus86@gmail.com>
2026-02-05 23:43:11 +0200

    chore: update the events create dto with ticket tiers array

--- src/components/events/dto/create-event.dto.ts
+++ src/components/events/dto/create-event.dto.ts
@@ -1,4 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { CreateTicketTierDto } from 'src/components/ticket_tiers/dto/create-ticket_tier.dto';
 
 export class CreateEventDto {
     @ApiProperty({ example: 'Unima Music Festival', description: 'Title of the event' })
@@ -18,5 +19,7 @@ export class CreateEventDto {
 
     @ApiProperty({ example: 1, description: 'ID of the organizer creating the event' })
     organizerId: string;
+
+    tiers: CreateTicketTierDto[]
 
 }
