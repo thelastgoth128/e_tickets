9a3d66c7be830c08df6078ae218f2c9a5e575267
thelastgoth128 <thindwacyrus86@gmail.com>
2026-02-06 22:52:56 +0200

    feat: wire up tickettier entity

--- src/components/transactions/transactions.module.ts
+++ src/components/transactions/transactions.module.ts
@@ -4,11 +4,14 @@ import { TransactionsService } from './transactions.service';
 import { TransactionsController } from './transactions.controller';
 import { Transaction } from './entities/transaction.entity';
 import { HttpModule } from '@nestjs/axios';
+import { TicketTier } from '../ticket_tiers/entities/ticket_tier.entity';
+import { TicketsModule } from '../tickets/tickets.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Transaction]),
-    HttpModule
+    TypeOrmModule.forFeature([Transaction, TicketTier]),
+    HttpModule,
+    TicketsModule,
   ],
   controllers: [TransactionsController],
   providers: [TransactionsService],
