6d9733bfbe3bb9ae1304e8f59f756639d81a519f
thelastgoth128 <thindwacyrus86@gmail.com>
2026-02-06 22:26:28 +0200

    feat: add capacity and payment to dto

--- src/components/tickets/dto/create-ticket.dto.ts
+++ src/components/tickets/dto/create-ticket.dto.ts
@@ -1,4 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { CreateTransactionDto } from 'src/components/transactions/dto/create-transaction.dto';
 
 export class CreateTicketDto {
     @ApiProperty({ example: 'uuid-event-123', description: 'ID of the event' })
@@ -9,6 +10,12 @@ export class CreateTicketDto {
 
     @ApiProperty({ example: 'uuid-user-123', description: 'ID of the user purchasing the ticket (optional)', required: false })
     userId?: string;
+
+    @ApiProperty({ example: '1', description: "any number of tickets, not over the available", required: false })
+    capacity: number;
+
+    @ApiProperty({ example: "payment amount", description: "The total payment amount for the ticket(s)" })
+    payments: CreateTransactionDto[]
 
     @ApiProperty({ example: 1, description: 'ID of the buyer purchasing the ticket (legacy support)', required: false })
     buyerId?: number;
